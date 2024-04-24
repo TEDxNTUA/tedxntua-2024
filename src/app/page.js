d046cafe411c2f687edb43f9ccfe4a6ac194c469
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2024-04-24 23:18:54 +0300

    add footer and components used on footer items. The magnetic component and the rounded button

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import NewFooter from "@/components/Footer/NewFooter";
+
 function HomePage() {
   return (
     <main>
       <span className="text-xl text-white mt-[50vh]">Main page</span>
+      <NewFooter />
     </main>
   );
 }
---
+++ src/components/Footer/SocialMediaItem.js
@@ -0,0 +1,37 @@
+"use client";
+import { useState } from "react";
+
+function SocialMediaItem({ iconName, link, color, hoverColor }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <a
+      href={link}
+      target="_blank"
+      className="max-w-[50%] sm:max-w-[30%] md:max-w-[60%] inline-block"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div>
+        {/* <Image */}
+        <img
+          className={`${isHovered ? "hidden" : "block"}`}
+          src={`./socialMediaIcons/${color}/${iconName}-${color}.png`}
+          alt={`${iconName}-icon`}
+          width={30}
+          height={30}
+        />
+        {/* <Image */}
+        <img
+          className={`${isHovered ? "block" : "hidden"}`}
+          src={`./socialMediaIcons/${hoverColor}/${iconName}-${hoverColor}.png`}
+          alt={`${iconName}-icon`}
+          width={30}
+          height={30}
+        />
+      </div>
+    </a>
+  );
+}
+
+export default SocialMediaItem;
