e758ccf0836aa46efa81795a08544169e351adef
john-papani <johnpapani1@gmail.com>
2024-05-07 18:45:28 +0300

    Minor changes modal

--- src/components/Modal/ModalContent.js
+++ src/components/Modal/ModalContent.js
@@ -1,6 +1,6 @@
 const ModalContent = ({ name, datalist }) => {
   const entity = datalist.find((entity) => entity.fullname === name);
-  console.log(entity);
+  // console.log(entity);
   if (!entity) {
     return <div>Not found</div>;
   }
@@ -8,18 +8,21 @@ const ModalContent = ({ name, datalist }) => {
   return (
     <>
       {/* Mobile version */}
-      <div className="h-auto md:hidden flex flex-col items-center justify-center my-7 w-[90%] mx-auto">
-        <p className="text-2xl text-our-purple font-bold italic underline underline-offset-8 pb-2">
+      <div className="h-auto lg:hidden flex flex-col items-center justify-center my-7 w-[90%] mx-auto">
+        <p className="text-2xl text-our-purple font-bold italic underline underline-offset-8 pb-1 text-center">
           {entity.fullname}
         </p>
-        {entity.title && (
-          <p className="py-2 font-bold text-center">{entity.title}</p>
+        {entity.type && (
+          <p className="font-semibold italic pb-1 text-sm text-center">{entity.type}</p>
         )}
         <img
           className="h-[20vh] rounded-full shadow-lg border-4 border-white"
           src={entity.photo}
           alt={entity.fullname}
         />
+        {entity.title && (
+          <p className="pt-3 font-bold text-base text-center">{entity.title}</p>
+        )}
         <p className="text-justify my-5 w-[95%] 4xl:w-[70%] mx-auto lg:text-lg text-sm max-h-[35vh] h-auto overflow-y-auto border border-our-yellow rounded p-2 flex justify-center">
           {entity.bio}
         </p>
@@ -40,18 +43,23 @@ const ModalContent = ({ name, datalist }) => {
       </div>
 
       {/* Laptop version */}
-      <div className="hidden md:flex flex-row items-center justify-center my-7 h-auto w-[90%] mx-auto">
+      <div className="hidden lg:flex flex-row items-center justify-center my-7 h-auto w-[90%] mx-auto">
         <img
           className="h-[40vh] w-[40vh] rounded-full object-top object-cover shadow-lg border-4 border-white"
           src={entity.photo}
           alt={entity.fullname}
         />
         <div className="flex flex-col items-center justify-center">
-          <p className="text-2xl text-our-purple font-bold italic underline underline-offset-8">
+          <p className="text-2xl text-our-purple font-bold italic underline underline-offset-8 text-center">
             {entity.fullname}
           </p>
+          {entity.type && (
+            <p className="py-1 font-semibold italic text-center">
+              {entity.type}
+            </p>
+          )}
           {entity.title && (
-            <p className="py-2 font-bold italic text-center">{entity.title}</p>
+            <p className="pt-3 font-bold text-xl text-center">{entity.title}</p>
           )}
 
           <p className="text-justify my-5 w-[80%] 4xl:w-[70%] mx-auto max-h-[65vh] overflow-y-auto lg:text-base text-sm p-2">
