b30e9a466d03dae1a412c4d0e61568f9599c2899
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2024-05-08 22:34:47 +0300

    add main page. Finished for desktop

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,8 @@ import dynamic from "next/dynamic";
 import Preloader from "../components/Preloader/Preloader";
 import Paragraph from "@/components/Paragraph/Paragraph";
 import ImageSlider from "@/components/ImageSlider";
+import ImageGrid from "@/components/ImageGrid";
 import Kakogiannis from "@/components/Kakogiannis/Kakogiannis";
-
-import NewFooter from "@/components/Footer/NewFooter";
 
 const Scene = dynamic(() => import("@/components/Torus/Scene"), {
   // loading: () => <p>Loading...</p>,
@@ -17,6 +16,7 @@ const Scene = dynamic(() => import("@/components/Torus/Scene"), {
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(true); UNCOMMENT FOR PRELOADRE
 
   useEffect(() => {
     (async () => {
@@ -38,37 +38,85 @@ export default function Home() {
     "Η εφημερία αποτελεί την αιτία, λόγω της οποίας όλα εξυψώνονται σε ένα βάθρο. Δεν είναι λίγες οι φορές που εφήμερα γεγονοτα αποδεικνύονται καθοριστικά (life changing)… Είναι δύσκολο να αποδεχθούμε την εφήμερη φύση όσων διαδραματίζονται γύρω μας ή και μέσα μας. Ωστόσο, όλα αλλάζουν και είναι εξαιρετικά σημαντικό να συμφιλιωθεί κανείς με την παροδικότητα που τον περιβάλλει. «Some ephemeral moments must be given a memory. The temporality of an instant may radiate a twinkle of eternity»";
 
   return (
-    <main className="bg-black">
+    <main className="bg-black max-w-[2040px] mx-auto">
       <AnimatePresence mode="wait">
         {isLoading && <Preloader />}
       </AnimatePresence>
-      <div className="bg-black">
-        <div className="first-page-content flex flex-col lg:flex-row h-[calc(100vh-65px)] mt-[65px] mx-auto w-[90%] xl:w-[80%] max-w-[100rem]">
-          {/* <div className="lg:w-1/2"> */}
-          <Scene /> {/* Donut */}
-          {/* </div> */}
-          <div className="hidden lg:flex justify-center items-center">
-            <div
-              data-scroll
-              data-scroll-speed={0.5}
-              className="flex flex-col gap-2 text-2xl m-auto text-white tracking-wider"
-            >
-              <p>What is ephemeral may not be the object itself,</p>
-              <p>but the attention we afford it and how much we are involved</p>
-            </div>
+      {/* <div className="bg-black"> */}
+      <div
+        style={{
+          backgroundImage: `url('/background/background.jpg')`,
+          backgroundSize: "cover", // Optional: adjust as needed
+        }}
+        className="1st-scroll bg-fixed h-[calc(100vh-65px)] max-h-[900px] grid grid-cols-1 lg:grid-cols-2 mx-auto w-full"
+      >
+        {/* <div className="first-page-content flex flex-col lg:flex-row h-[calc(100vh-65px)] mt-[65px] mx-auto w-[90%] xl:w-[80%] max-w-[100rem]"> */}
+
+        <div
+          style={{
+            backgroundImage: `url('/design/grid.png')`,
+            backgroundSize: "", // Optional: adjust as needed
+          }}
+          className="hidden lg:flex justify-center items-center w-[600px] mt-10 ml-4"
+        >
+          <div
+            data-scroll
+            data-scroll-speed={0.5}
+            className="flex flex-col gap-2 text-2xl m-auto text-white tracking-wider w-3/5"
+          >
+            <p>What is ephemeral may not be the object itself,</p>
+            <p>but the attention we afford it and how much we are involved</p>
           </div>
         </div>
+        <Scene />
+        {/* </div> */}
       </div>
 
-      <div className="py-[20vh] text-xl text-white leading-6 text-justify">
-        <div className="w-[70%] lg:[50%] mx-auto">
-          <Paragraph paragraph={paragraph1} />
+      <div
+        style={{
+          backgroundImage: `url('/background/background.jpg')`,
+          backgroundSize: "cover", // Optional: adjust as needed
+        }}
+        className="2nd-scroll bg-fixed h-[calc(100vh)] max-h-[900px] grid grid-cols-1 lg:grid-cols-2 mx-auto w-full"
+      >
+        <div
+          style={{
+            backgroundImage: `url('/design/grid.png')`,
+            backgroundSize: "", // Optional: adjust as needed
+          }}
+          className="hidden lg:flex justify-center items-center w-[600px] mb-10 ml-4"
+        >
+          <img src="/design/apple.png" alt="bitten-apple" className="w-1/2" />
+        </div>
+
+        <div className="flex flex-row justify-between items-center gap-12  pr-28">
+          <div
+            style={{
+              backgroundImage: `url('/design/purple-rect-small.png')`,
+              backgroundSize: "cover", // Optional: adjust as needed
+            }}
+            className="w-[80%] max-w-[1000px] rounded-sm"
+          >
+            <div className="mx-auto text-white leading-tight break-words text-md p-10">
+              <Paragraph paragraph={paragraph1} />
+            </div>
+          </div>
+          <img
+            src="/design/embrace-feeling.png"
+            alt="embrace-feeling-text"
+            className="h-[40%] my-auto max-h-[300px]"
+          />
         </div>
       </div>
 
       {/* <Description /> */}
       {/* <Projects /> */}
-      <ImageSlider />
+      <div className="3xl:hidden">
+        <ImageSlider />
+      </div>
+      <div className="hidden 3xl:block">
+        <ImageGrid />
+      </div>
       {/* <Contact /> */}
       <Kakogiannis />
       {/* <NewFooter /> */}
