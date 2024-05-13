9668bb233f2c6a5410858d721fe48a80768b7012
john-papani <johnpapani1@gmail.com>
2024-05-13 22:11:55 +0300

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: false,
+  };
+  
+  module.exports = nextConfig;
